7b13578e6d1d37b43b7eeab98dcc8f476f5b65c2
Dulgiduck <jsh@sngy.io>
2022-03-24 10:58:33 +0900

    스프링 데이터 페이지 활용2 - CountQuery 최적화

--- src/main/java/topia/duck/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/topia/duck/querydsl/repository/MemberRepositoryImpl.java
@@ -3,10 +3,12 @@ package topia.duck.querydsl.repository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import topia.duck.querydsl.dto.MemberSearchCondition;
 import topia.duck.querydsl.dto.MemberTeamDto;
@@ -139,7 +141,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .select(QMember.member)
                 .from(QMember.member)
                 .leftJoin(QMember.member.team, QTeam.team)
@@ -148,10 +150,11 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                )
-                .fetchCount();
+                );
 
-        return new PageImpl<>(content, pageable, total);
+
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+//        return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression usernameEq(String username) {
