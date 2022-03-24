52881e6b756217a5eb00e91d34618b3ea0e6d128
Dulgiduck <jsh@sngy.io>
2022-03-24 10:30:27 +0900

    조회 API 컨트롤러 개발

---
+++ src/main/java/topia/duck/querydsl/controller/MemberController.java
@@ -0,0 +1,21 @@
+package topia.duck.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import topia.duck.querydsl.dto.MemberSearchCondition;
+import topia.duck.querydsl.dto.MemberTeamDto;
+import topia.duck.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition){
+        return memberJpaRepository.search(condition);
+    }
+}
