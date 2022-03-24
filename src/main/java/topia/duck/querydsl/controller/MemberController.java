038313935d28dd9b0a045cefb7f727ca2ab297f6
Dulgiduck <jsh@sngy.io>
2022-03-24 11:03:30 +0900

    스프링 데이터 페이징 활용3 - 컨트롤러 개발

--- src/main/java/topia/duck/querydsl/controller/MemberController.java
+++ src/main/java/topia/duck/querydsl/controller/MemberController.java
@@ -1,11 +1,15 @@
 package topia.duck.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import topia.duck.querydsl.dto.MemberSearchCondition;
 import topia.duck.querydsl.dto.MemberTeamDto;
 import topia.duck.querydsl.repository.MemberJpaRepository;
+import topia.duck.querydsl.repository.MemberRepository;
+import topia.duck.querydsl.repository.MemberRepositoryCustom;
 
 import java.util.List;
 
@@ -13,9 +17,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition){
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
